724feaa9d980a7a2a63d8aeed3cc685ac742c8fd
Ricardo Mazzuco <ricardo.mazzuco@neomind.com.br>
2026-08-27 17:25:04 -0300

    homepage criada e estilizada

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,13 @@
+import "./StartScreen.css"
+
+const StartScreen = () => {
+  return (
+    <div className="startScreenDiv">
+        <h1>Secret Word</h1>
+        <p>Clique no botão a baixo para começar a jogar</p>
+        <button>Começar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
---
+++ src/data/words.jsx
@@ -0,0 +1,12 @@
+export const wordsList = {
+  carro: ["motor", "volante", "freio", "pneu", "cambio"],
+  casa: ["cozinha", "quarto", "sala", "banheiro", "garagem"],
+  comida: ["arroz", "feijao", "pao", "queijo", "fruta"],
+  esporte: ["futebol", "natacao", "tenis", "corrida", "volei"],
+  animal: ["cachorro", "gato", "passaro", "peixe", "cavalo"],
+  natureza: ["rio", "montanha", "floresta", "praia", "nuvem"],
+  tecnologia: ["computador", "teclado", "monitor", "internet", "servidor"],
+  escola: ["livro", "prova", "aluno", "professor", "caderno"],
+  musica: ["guitarra", "bateria", "piano", "microfone", "melodia"],
+  clima: ["chuva", "vento", "sol", "neve", "tempestade"]
+};
